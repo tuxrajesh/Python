b398d74c865d43216e0dd4d492aa7375c5da872c
Rajeshwaran SP <tuxrajesh@gmail.com>
2024-03-11 10:59:45 -0700

    Added the Flip Card - timer logic

--- 100DaysOfCode/Day31/main.py
+++ 100DaysOfCode/Day31/main.py
@@ -11,21 +11,24 @@ current_card = {}
 def flip_card():
     global current_card    
     canvas.itemconfig(flash_card, image=card_back_img)
-    canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_title, cnf={"text":"English", "fill":"white" })
+    canvas.itemconfig(card_word, cnf={"text":current_card["English"], "fill":"white" })
     pass
 
-def get_next_word():
-    global current_card 
+def get_next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(word_dictionaries)
-    canvas.itemconfig(flash_card, image=card_back_img)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(flash_card, image=card_front_img)
+    canvas.itemconfig(card_title, cnf={"text":"French", "fill":"black" })
+    canvas.itemconfig(card_word, cnf={"text":current_card["French"], "fill":"black" })
+    flip_timer = window.after(3000, flip_card)
     pass
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, flip_card)
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file=".\\images\\card_front.png")
@@ -36,13 +39,12 @@ card_word = canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold")
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_img = PhotoImage(file=".\\images\\wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=get_next_card)
 wrong_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file=".\\images\\right.png")
-right_button = Button(image=right_img, highlightthickness=0, command=get_next_word)
+right_button = Button(image=right_img, highlightthickness=0, command=get_next_card)
 right_button.grid(row=1, column=1)
 
-get_next_word()
-window.after(3000, flip_card)
+get_next_card()
 window.mainloop()
